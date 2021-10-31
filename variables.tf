93b1bf24384e4ad6f04101c02703ed5b5e583762
Rosana RuFer <rosanarufer@gmail.com>
2021-10-31 20:32:04 +0000

    Terraform is fun

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "rosanarufer"
+}
+
+variable "credentials_file" {
+  default = "service-account-credentials.json"
+}
+
+variable "region" {
+  default = "europe-west2"
+}
+
+variable "zone" {
+  default = "europe-west2-c"
+}
